f825268753b279f8235a11941c132c00bbb3b490
maxpower24 <maxpower24+github@pm.me>
2021-08-28 00:04:42 +1000

    update script fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,11 @@
 # Clone dotfiles repo
 local_repo=~/shizen-os
 origin="https://github.com/maxpower24/shizen-os.git"
-cd $local_repo && git init && git fetch $origin && git checkout -b main $origin && git pull $origin main
+cd $local_repo
+git init
+git fetch $origin
+git checkout -b main $origin
+git pull $origin main
 
 # Copy config and other dotfile data
 cp -r $local_repo/wallpapers ~/pics
